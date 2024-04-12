35203c4f8a150a7568465451fc7426b14d7e6e01
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2024-04-12 08:56:51 +0700

    New SW

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-workbox.precaching.precacheAndRoute([
-  { url: '/', revision: null },
-]);
+
 workbox.routing.registerRoute(
   new workbox.routing.NavigationRoute(
-    new workbox.strategies.NetworkFirst()
+    new workbox.strategies.StaleWhileRevalidate()
   )
 );
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== workbox.core.cacheNames.runtime) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
