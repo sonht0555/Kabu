ed9fa9371ea4b0644e480f7c6f6a6038c578808e
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2024-04-12 23:12:28 +0700

    New shader

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 workbox.setConfig({ debug: false });
-let revision = '5';
+let revision = '6';
 revision = (parseInt(revision) + 1).toString();
 
 workbox.precaching.precacheAndRoute([
