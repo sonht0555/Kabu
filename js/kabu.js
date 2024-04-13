d5caea075b6a1cf476fe342454fa8b90be9dd79b
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2024-04-13 19:30:36 +0700

    .

--- js/kabu.js
+++ js/kabu.js
@@ -149,7 +149,7 @@ async function loadGame(gameName) {
             await turboF(turboState);
         }
         await delay(1500);
-        await notiMessage("Kabu v1.02", 3000);
+        await notiMessage("Kabu v1.03", 3000);
         setInterval(() => {saveStatePeriodically()}, 60000);
         setInterval(() => {saveStateInCloud()}, 3600000);
     } catch (error) {
@@ -446,21 +446,19 @@ function LoadstateInPage(saveSlot, divs, dateState) {
     const imageStateDiv = document.getElementById(divs);
     const getNameRom = localStorage.getItem("gameName");
     imageStateDiv.onclick = () => {
-        const stateList = document.getElementById("stateList");
-        stateList.classList.toggle("visible");
-        statePageButton.classList.toggle("active");
-        led(saveSlot);
-        notiMessage(`Loaded State in Slot [${saveSlot}]`, 2000);
-        setTimeout(() => {
-            loadState(saveSlot);
-            localStorage.setItem("slotStateSaved", saveSlot)
-        }, 100);
-    };
-    imageStateDiv.addEventListener("touchstart", function() {
-        const stateName = getNameRom.replace(".gba", `.ss${saveSlot}`);
-        clearTimeout(clickTimer);
-        clickTimer = setTimeout(function() {
-            console.log(stateName)
+        if (confirm("Do you want to load save state?")) {
+            const stateList = document.getElementById("stateList");
+            stateList.classList.toggle("visible");
+            statePageButton.classList.toggle("active");
+            led(saveSlot);
+            notiMessage(`Loaded State in Slot [${saveSlot}]`, 2000);
+            setTimeout(() => {
+                loadState(saveSlot);
+                localStorage.setItem("slotStateSaved", saveSlot)
+            }, 100);
+        } else {
+            const stateName = getNameRom.replace(".gba", `.ss${saveSlot}`);
+            clearTimeout(clickTimer);
             Module.deleteFile(`/data/states/${stateName}`);
             localStorage.removeItem(`${getNameRom}_dateState${saveSlot}`);
             localStorage.removeItem(`${getNameRom}_imageState${saveSlot}`);
@@ -475,8 +473,8 @@ function LoadstateInPage(saveSlot, divs, dateState) {
                 imageStateDiv.appendChild(image);
                 document.getElementById(dateState).textContent = date;
             }, 100);
-        }, 3000);
-    });
+        }
+    };
     while (imageStateDiv.firstChild) {
         imageStateDiv.removeChild(imageStateDiv.firstChild);
     }
