27f51beb3d8efb1349e8d031316ff006c63cbb8b
Son Hoang Tuan <hades190791@gmail.com>
2024-05-01 08:54:16 +0700

    v1.29

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 workbox.setConfig({ debug: false });
-let revision = '28';
+let revision = '29';
 revision = (parseInt(revision) + 1).toString();
 
 workbox.precaching.precacheAndRoute([
