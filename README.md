b8ed310bef5d094e5fb99497566466f6979156fd
Son Hoang Tuan <hades190791@gmail.com>
2024-06-24 11:31:56 +0700

    v1.57
    Update SW

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Kabu
  Gba & Gbc Emulator online.
+## Link Online
+ https://sonht0555.github.io/Kabu/
--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js');
 workbox.setConfig({ debug: false });
-let revision = '54';
+let revision = '57';
 revision = (parseInt(revision) + 1).toString();
 
 workbox.precaching.precacheAndRoute([
   { url: './css/main.css', revision: revision },
-  { url: './font/04b03b.ttf', revision: revision },
+  { url: './font/04b.ttf', revision: revision },
+  { url: './font/04bf.ttf', revision: revision },
   { url: './font/Tuson.ttf', revision: revision },
   { url: './img/favi.png', revision: revision },
   { url: './img/icon.png', revision: revision },
