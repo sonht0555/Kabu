fc8f3a8a889da3c4da2542ce578a7b369dd77867
Son Hoang Tuan <hades190791@gmail.com>
2024-07-12 16:58:41 +0700

    v1.96
    new Emulator

--- js/kabu.js
+++ js/kabu.js
@@ -1,6 +1,6 @@
 import { startGBA } from "./initialize.js";
 //import { taskA } from "./cloud.js";
-let gameVer = 'V1.95';
+let gameVer = 'V1.956';
 let turboState = 1;
 let clickState = 0;
 let countAutoSave = 0;
