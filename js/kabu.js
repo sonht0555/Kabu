b9efe7c9b0b2c3d268962830d3963da5818c4502
Son Hoang Tuan <hades190791@gmail.com>
2024-07-22 09:38:04 +0700

    v2.18

--- js/kabu.js
+++ js/kabu.js
@@ -8,7 +8,7 @@ let Module = null;
 window.addEventListener("gbaInitialized", (event) => {
     Module = event.detail.Module;
 });
-let gameVer = 'V2.17';
+let gameVer = 'V2.18';
 let turboState = 1;
 let clickState = 0;
 let clickTurbo = 0
