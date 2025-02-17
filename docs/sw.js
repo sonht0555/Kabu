12d918d553b738282804f4fa806fae5314e30604
Son Hoang Tuan <hades190791@gmail.com>
2025-02-17 10:19:48 +0700

    v2.56

--- docs/sw.js
+++ docs/sw.js
@@ -1,11 +1,47 @@
-let revision = 'V2.55';
+let revision = 'V2.56';
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.3.0/workbox-sw.js');
 workbox.setConfig({ debug: false });
 revision = (parseInt(revision) + 1).toString();
+
+// IndexedDB Helper
+const dbName = 'cacheDB';
+const storeName = 'resources';
+
+function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName, 1);
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains(storeName)) {
+        db.createObjectStore(storeName, { keyPath: 'url' });
+      }
+    };
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+async function saveToDB(url, response) {
+  const db = await openDB();
+  const transaction = db.transaction(storeName, 'readwrite');
+  const store = transaction.objectStore(storeName);
+  store.put({ url, response: await response.clone().arrayBuffer() });
+}
+
+async function getFromDB(url) {
+  const db = await openDB();
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(storeName, 'readonly');
+    const store = transaction.objectStore(storeName);
+    const request = store.get(url);
+    request.onsuccess = () => resolve(request.result ? new Response(request.result.response) : null);
+    request.onerror = () => reject(request.error);
+  });
+}
+
 workbox.precaching.precacheAndRoute([
   { url: './src/css/main.css', revision: revision },
   { url: './src/font/04b.ttf', revision: revision },
-  { url: './src/font/04bf.ttf', revision: revision },
   { url: './src/font/Tuson.ttf', revision: revision },
   { url: './img/favi.png', revision: revision },
   { url: './img/icon.png', revision: revision },
@@ -29,16 +65,14 @@ workbox.precaching.precacheAndRoute([
 
 workbox.routing.registerRoute(
   /\.(?:css|ttf|png|js|wasm|html|json)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'static-resources',
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        // maxEntries: 150,
-        // purgeOnQuotaError: true,
-        maxAgeSeconds: 1 * 24 * 60 * 60,
-      }),
-    ],
-  })
+  async ({ request }) => {
+    const cachedResponse = await getFromDB(request.url);
+    if (cachedResponse) return cachedResponse;
+    
+    const networkResponse = await fetch(request);
+    saveToDB(request.url, networkResponse.clone());
+    return networkResponse;
+  }
 );
 
 self.addEventListener('message', async (event) => {
@@ -48,6 +82,9 @@ self.addEventListener('message', async (event) => {
       if (response.ok) {
         const cacheNames = await caches.keys();
         await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+        const db = await openDB();
+        const transaction = db.transaction(storeName, 'readwrite');
+        transaction.objectStore(storeName).clear();
       }
     } catch (error) {}
   }
