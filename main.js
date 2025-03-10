83664ae8e3a6a0f2de0fe3ef0f09d92458b20495
Son Hoang Tuan <hades190791@gmail.com>
2025-03-10 13:27:27 +0700

    Test

---
+++ main.js
@@ -0,0 +1,20 @@
+import mGBA_v2 from "./mgba.js";
+/*/ --------------- Initialization ----------- */
+const Module = {canvas: document.getElementById("canvas")};
+function initializeCore(coreInitFunction, module) {
+    coreInitFunction(module).then(function(module) {
+        module.FSInit();
+    });
+}
+
+initializeCore(mGBA_v2, Module);
+setTimeout(() => {
+Module.loadGame(`/data/games/Goodboy Galaxy.zip`);
+}, 1000);
+
+setTimeout(() => {
+    console.log(Module);
+    console.log(Object.keys(Module)); // Xem các thuộc tính
+    console.log(Module.renderer); 
+    console.log(Module);
+}, 3000);
