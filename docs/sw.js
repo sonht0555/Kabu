25520d3e0df1144e88ade21c5189551dac496ede
Son Hoang Tuan <hades190791@gmail.com>
2025-03-14 20:23:22 +0700

    --- Build V3.42 ---

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-let revision = 'V3.41';
+let revision = 'V3.42';
 var urlsToCache = [
     '/',
     './src/css/main.css',
--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,7 @@ document.getElementById("GBAver").addEventListener("click", () => {
     setTimeout(() => { window.location.reload(); }, 1000);
 });
 /*/ --------------- Initialization ----------- */
-const Module = {canvas: document.getElementById("canvas")};
+const Module = {canvas: document.getElementById("canvas-1")};
 function initializeCore(coreInitFunction, module) {
     coreInitFunction(module).then(function(module) {
         module.FSInit();
@@ -128,9 +128,12 @@ export async function loadGame(romName) {
         }
     } else {
         await Module.loadGame(`/data/games/${romName}`);
-        await shaderData();
+        setTimeout(() => {
+        shaderData();
+        }, 4000);
     }
     // show status ingame
+    Dslay();
     await statusShow();
 }
 export async function saveState(slot) {
@@ -362,3 +365,48 @@ export async function FSSync() {
     Module.FSSync();
 }
 export const rewind = (type) => Module.toggleRewind?.(type) || null;
+
+export function Dslay() {
+    const bufferCanvas = document.createElement("canvas");
+    bufferCanvas.width = 240;
+    bufferCanvas.height = 160;
+    const bufferCtx = bufferCanvas.getContext("2d");
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 240; // Giữ nguyên kích thước gốc
+    canvas.height = 160;
+
+    // Scale bằng CSS thay vì vẽ trực tiếp
+    //canvas.style.transform = "scale(1.5)";
+    //canvas.style.transformOrigin = "top left";
+    //canvas.style.imageRendering = "pixelated";
+    
+    function updateFrame() {
+        const pixelData = Module.getPixelData();
+        const imageData = bufferCtx.createImageData(240, 160);
+
+        for (let i = 0; i < pixelData.length; i++) {
+            const color = pixelData[i];
+            const index = i * 4;
+            
+            imageData.data[index] = (color & 0xFF);          // Blue → Red
+            imageData.data[index + 1] = (color >> 8) & 0xFF; // Green (không đổi)
+            imageData.data[index + 2] = (color >> 16) & 0xFF;// Red → Blue
+            imageData.data[index + 3] = 255;                 // Alpha luôn là 255
+        }
+        bufferCtx.putImageData(imageData, 0, 0);
+        
+        // Tắt làm mịn pixel
+        ctx.imageSmoothingEnabled = false;
+        ctx.webkitImageSmoothingEnabled = false;
+        ctx.mozImageSmoothingEnabled = false;
+        ctx.msImageSmoothingEnabled = false;
+        ctx.oImageSmoothingEnabled = false;
+        
+        ctx.drawImage(bufferCanvas, 0, 0, 240, 160); // Giữ nguyên size khi vẽ
+
+        requestAnimationFrame(updateFrame);
+    }
+    requestAnimationFrame(updateFrame);
+};
